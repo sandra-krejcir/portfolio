372c25f31b9d155a4435c66def0726994c370dff
Sandra Krejcir <krejcir.sandra@gmail.com>
2021-10-19 20:41:14 +0200

    made the intro buttons work

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 let slideIndex = 1;
-showSlides(slideIndex);
 
 function nextSlide(n) {
   showSlides((slideIndex += n));
@@ -10,9 +9,12 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
+  document.querySelector(".frontground-container").classList.add("hide");
+  document.querySelector(".slideshow-container").classList.remove("hide");
   let i;
   const slides = document.getElementsByClassName("mySlides");
   const dots = document.getElementsByClassName("dot");
+
   if (n > slides.length) {
     slideIndex = 1;
   }
@@ -27,4 +29,69 @@ function showSlides(n) {
   }
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
+}
+
+let maxFirstIterations;
+let maxSecondIterations;
+let iterator;
+const theTextOne = document.querySelector(".typewritten").textContent.trim();
+const theTextTwo = document.querySelector(".typewritteN").textContent.trim();
+
+setTimeout(init, 1000);
+
+function init() {
+  console.log("function init()");
+  maxFirstIterations = theTextOne.length + 1;
+  maxSecondIterations = theTextTwo.length + 1;
+  iterator = 0;
+
+  document.querySelector("#typewriter").innerHTML = "";
+  loop();
+}
+
+function loop() {
+  console.log("function loop()", iterator);
+  console.log("theTextOne[iterator]", theTextOne[iterator]);
+  iterator++;
+  document.querySelector(".typewritten").classList.remove("hide");
+  document.querySelector("#theWriter").classList.remove("hide");
+
+  if (iterator < maxFirstIterations) {
+    document.querySelector("#typewriter").textContent +=
+      theTextOne[iterator - 1];
+    document.querySelector("#theWriter").classList.remove("hide");
+    setTimeout(loop, 170);
+  } else {
+    document.querySelector("#theWriter").classList.add("hide");
+    secondInit();
+  }
+}
+
+function secondInit() {
+  maxSecondIterations = theTextTwo.length + 1;
+  iterator = 0;
+  document.querySelector(".typewritteN").classList.remove("hide");
+  document.querySelector("#theWriteR").classList.remove("hide");
+
+  document.querySelector("#typewriteR").innerHTML = "";
+  secondLoop();
+}
+
+function secondLoop() {
+  iterator++;
+
+  if (iterator < maxSecondIterations) {
+    document.querySelector("#typewriteR").textContent +=
+      theTextTwo[iterator - 1];
+    document.querySelector("#theWriteR").classList.remove("hide");
+    setTimeout(secondLoop, 150);
+  } else {
+    document.querySelector("#theWriteR").classList.add("hide");
+    revealButtons();
+  }
+}
+
+function revealButtons() {
+  document.querySelector("#contact").classList.add("showFirst");
+  document.querySelector("#viewing").classList.add("showSecond");
 }
